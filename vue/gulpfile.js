9f97fa77e966f527ca3e672778ad3d618f2c8572
Eduardo San Martin Morote <i@posva.net>
2015-10-03 16:54:25 +0200

    Initial vue talk

---
+++ vue/gulpfile.js
@@ -0,0 +1,153 @@
+'use strict';
+
+var pkg = require('./package.json'),
+  gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  plumber = require('gulp-plumber'),
+  rename = require('gulp-rename'),
+  connect = require('gulp-connect'),
+  browserify = require('gulp-browserify'),
+  uglify = require('gulp-uglify'),
+  jade = require('gulp-jade'),
+  stylus = require('gulp-stylus'),
+  autoprefixer = require('gulp-autoprefixer'),
+  csso = require('gulp-csso'),
+  del = require('del'),
+  through = require('through'),
+  opn = require('opn'),
+  path = require('path'),
+  imagemin = require('gulp-imagemin'),
+  webshot = require('webshot'),
+  taskListing = require('gulp-task-listing'),
+  isDist = process.argv.indexOf('serve') === -1;
+
+gulp.task('help', taskListing);
+
+gulp.task('webshot', function(done) {
+  connect.server({
+    root: 'dist',
+    port: 8888
+  });
+  webshot('http://localhost:8888', 'dist/thumbail.png', function(err) {
+    if (err)
+      console.error('Error webshotting', err);
+    connect.serverClose();
+    done();
+  });
+});
+
+gulp.task('js', ['clean:js'], function() {
+  return gulp.src('src/scripts/main.js')
+    .pipe(isDist ? through() : plumber())
+    .pipe(browserify({
+      debug: !isDist
+    }))
+    .pipe(isDist ? uglify() : through())
+    .pipe(rename('build.js'))
+    .pipe(gulp.dest('dist/build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('common', ['clean:common'], function() {
+  return gulp.src('../common/**/*')
+    .pipe(isDist ? through() : plumber())
+    .pipe(gulp.dest('dist/common'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', ['clean:html'], function() {
+  return gulp.src('src/index.jade')
+    .pipe(isDist ? through() : plumber())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', ['clean:css'], function() {
+  return gulp.src('src/styles/main.styl')
+    .pipe(isDist ? through() : plumber())
+    .pipe(stylus({
+      // Allow CSS to be imported from node_modules
+      'include css': true,
+      'paths': ['../node_modules']
+    }))
+    .pipe(autoprefixer('last 2 versions', {
+      map: false
+    }))
+    .pipe(isDist ? csso() : through())
+    .pipe(rename('build.css'))
+    .pipe(gulp.dest('dist/build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('images', ['clean:images'], function() {
+  return gulp.src('src/images/**/*')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(connect.reload());
+});
+
+gulp.task('imagemin', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('src/images'));
+});
+
+gulp.task('clean', function(done) {
+  del('dist', done);
+});
+
+gulp.task('clean:html', function(done) {
+  del('dist/index.html', done);
+});
+
+gulp.task('clean:js', function(done) {
+  del('dist/build/build.js', done);
+});
+
+gulp.task('clean:css', function(done) {
+  del('dist/build/build.css', done);
+});
+
+gulp.task('clean:images', function(done) {
+  del('dist/images', done);
+});
+
+gulp.task('clean:common', function(done) {
+  del('dist/common', done);
+});
+
+gulp.task('connect', ['build'], function() {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+});
+
+gulp.task('open', ['connect'], function(done) {
+  opn('http://localhost:8080', done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['src/**/*.jade', '../utils/*.jade'], ['html']);
+  gulp.watch('../common/**/*', ['common']);
+  gulp.watch(['src/styles/**/*.styl', '../utils/*.styl'], ['css']);
+  gulp.watch('src/images/**/*', ['images']);
+  gulp.watch([
+    'src/scripts/**/*.js',
+    'bespoke-theme-*/dist/*.js' // Allow themes to be developed in parallel
+  ], ['js']);
+});
+
+gulp.task('bundle', ['build'], function() {
+  return gulp.src('dist/**/*')
+    .pipe(gulp.dest(path.join('../dist', path.basename(path.resolve('.')))));
+});
+
+gulp.task('build', ['js', 'html', 'css', 'images', 'common']);
+
+gulp.task('serve', ['open', 'watch']);
+
+gulp.task('default', ['bundle']);
---
+++ vue/src/scripts/main.js
@@ -0,0 +1,33 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  nebula = require('bespoke-theme-nebula'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  hash = require('bespoke-hash'),
+  substeps = require('bespoke-substeps/dom'),
+  progress = require('bespoke-progress');
+
+// Bespoke.js
+bespoke.from('article', [
+  nebula(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  substeps(),
+  backdrop(),
+  hash(),
+  progress()
+]);
+
+// Prism syntax highlighting
+require('prismjs/components/prism-core');
+require('prismjs/components/prism-clike');
+require('prismjs/components/prism-javascript');
+require('prismjs/components/prism-coffeescript');
+require('prismjs/components/prism-css');
+require('prismjs/components/prism-stylus');
+require('prismjs/components/prism-jade');
+require('prismjs/components/prism-bash');
+require('prismjs/components/prism-git');
